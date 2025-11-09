7a6d8cc87c33a5eb7ee6568440153be84927657f
SantiagoCumpa <santiago.cumpa.dev@gmail.com>
2025-11-08 23:46:36 -0500

    nvim configuration changes
    added linter, formatter and autocompletion configuration

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,32 +1,11 @@
+-- Lazy plugin manager
+require("config.lazy")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+-- Mini deps plugin manager
+-- require("config.mini")
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
+-- Neovim Options
 require("config.options")
-require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "catppuccin" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
+
+-- Lsp Server
+require("config.lsp")
--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,4 +1,3 @@
-
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.expandtab = true
@@ -7,3 +6,27 @@ vim.opt.tabstop = 2
 vim.opt.termguicolors = true
 vim.opt.wrap = false
 
+--- show diagnostics
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.HINT] = "",
+		},
+	},
+	underline = true,
+	severity_sort = true,
+})
+
+--- change docker-compose type file
+vim.filetype.add({
+	filename = {
+		["docker-compose.yml"] = "yaml.docker-compose",
+		["docker-compose.yaml"] = "yaml.docker-compose",
+		["compose.yml"] = "yaml.docker-compose",
+		["compose.yaml"] = "yaml.docker-compose",
+	},
+})
